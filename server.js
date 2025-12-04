d843ddfd94f016b3a5bd249cf051e31582d30dff
gao-alessio <alessio.gao@myenaip.eu>
2025-12-04 12:55:02 +0000

    sarvale

---
+++ server.js
@@ -0,0 +1,28 @@
+//oggetto javascript
+
+const STEELSERIES_Diablo = {
+EAN : "EAN08138100798",
+Puntamento : "ONEDANE",
+CONNESSIONE : "Usb",
+Serie : "Diola",
+Risoluzione : "100 - 5.700 CPI/DPI"
+
+}
+
+//per aprire la cose ("serie")
+
+console.log(STEELSERIES_Diablo.Serie);
+
+const votiStudenti = {
+  Amantea: 7,
+  Casa: 9,
+  jedn: 2,
+  manmuf: 3,
+  nene: 3,
+  iejr: 4,
+  kodod: 0.3
+
+}
+
+
+console.log(votiStudenti.Amantea);
